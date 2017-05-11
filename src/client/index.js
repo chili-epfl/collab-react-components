540d55eea2d4f83c6a615d2891da28512831b46b
Dario Anongba Varela <dario.anongba@gmail.com>
2017-05-11 15:16:06 +0200

    Central point for client exports

---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Dario on 11.05.17.
+ */
+
+import CollabEditor from 'CollabEditor';
+import CollabForm from 'CollabForm';
+
+module.exports = {
+  Editor: CollabEditor,
+  Form: CollabForm
+};
